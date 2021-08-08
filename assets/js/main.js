fd317728642e06aadcf3d452379f522eb06d99b5
timoshevichaleks <alti2692@gmail.com>
2021-08-08 13:24:48 +0300

    Добавил плавную анимацию по сайту

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+
+  $('#navbar').onePageNav({
+    currentClass: 'active',
+    scrollThreshold: 0.3,
+    scrollSpeed: 750,
+    easing: 'swing'
+      // filter: ':not(.navbar-brand)'
+  });
+
+})(jQuery);
